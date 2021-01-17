849f4bb9370cc7b6b0abd4dd1dcc592b393a3b04
Akbal-21 <akbal153@hotmail.com>
2021-01-16 18:15:04 -0600

    Redaccion del inicio

--- hola/src/components/principal.js
+++ hola/src/components/principal.js
@@ -7,13 +7,17 @@ export const principal = () => {
                 <div className="card-header">Informacion</div>
                 <div className="card-body">
                     <h5 className="card-title">Objetivo</h5>
-                    <p className="card-text">Se tiene el objetivo de crear una Pagina echa con React, HTML 5, Servlet. A la
-                        vez de que sirve como complemento de todos los aprendizajes esperados a lo largo del curso</p>
+                    <p className="card-text">Crear una página web con los aprendizajes obtenidos en la materia
+                        tecnologías para la web, tales como los estilos, creación de un entorno gráfico creado con la
+                        herramientas que nos proporciona React, un ejemplo de esto fue la creación del NavBar el cual se
+                        ocupa react-router-dom, se implementó un conjunto de rutas públicas y privadas de tal forma que
+                        uno no puede acceder a algunas rutas sin estar logueado dando una seguridad extra al proyecto.
+                    </p>
                     <img
                         src="../images/logo.png"
                         width={250}
                         height={200}
-                        className={'principal_image'}
+                        alt="Logo del proyecto"
                     />
                 </div>
             </div>
